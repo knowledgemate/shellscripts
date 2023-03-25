62aa3959dab63e238f79ee3576e54fd939cbbc9f
knowledgemate <125232588+knowledgemate@users.noreply.github.com>
2023-03-25 20:15:46 +0530

    Update useradd.sh

--- useradd.sh
+++ useradd.sh
@@ -7,9 +7,8 @@ if [ $(id -u) -eq 0 ]; then
 	if [ $? -eq 0 ]; then
 		echo "$username exists!"
 		exit 1
-	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-		useradd -m -p $pass $username
+	else		
+		useradd $username
 		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
 	fi
 else
